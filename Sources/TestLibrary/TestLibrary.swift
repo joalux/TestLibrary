46054265e1712484b6f88e0d0383b504a85ffdd0
joakim <joakim.lundb@gmail.com>
2022-02-08 13:34:00 +0100

    Initial Commit

---
+++ Sources/TestLibrary/TestLibrary.swift
@@ -0,0 +1,10 @@
+public struct TestLibrary {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+    
+    public func hello() -> String {
+        return "Hello"
+    }
+}
